a030b773e42f246fe7f28d474b9641c4daec3b75
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-02-24 16:35:13 +0700

    Exercise 12.12

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAsync } = require("../redis/index");
 
 router.get("/", async (req, res) => {
-  const added_todos = await getAsync("added_todos");
+  const added_todos = Number(await getAsync("added_todos")) || 0;
   const state = {
     added_todos,
   };
